65abe8d00c339adfee0b18ee414ee13e71380275
Ibrahim Ruby <opeyemiaremu34@gmail.com>
2019-10-04 11:27:34 +0100

    First vue component

---
+++ resources/js/components/menu/MenuContainer.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Menu Container 2</div>
+
+                    <div class="card-body">
+                        I'm a menu container.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "MenuContainer.vue"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/modules/menu/MenuContainer2.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="app" class="container">
+        It worked!!!
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "MenuContainer2.vue",
+        props: ['items'],
+    }
+</script>
+
+<style scoped>
+
+</style>
